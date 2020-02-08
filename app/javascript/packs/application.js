d2e735931ad7c15ef55f7756e926f717913dc23b
Basile Quinchon <basile.quinchon@gmail.com>
2020-02-08 13:24:38 +0100

    new show carousel

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,18 +4,12 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { initFlatPickr } from "plugins/flatpickr.js";
 import { initRellax } from "plugins/rellax.js";
-import { initFlickity } from "plugins/flickity.js";
 import { initNavbar } from "plugins/init_navbar.js";
 
 initFlatPickr();
 
 if (document.querySelector('.sidebar-tibo')) {
   initNavbar();
-}
-
-
-if (document.querySelector('.carousel')) {
-  initFlickity();
 }
 
 if (document.querySelector('.rellax')) {
