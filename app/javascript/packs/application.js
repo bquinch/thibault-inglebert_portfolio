3195e2cf5c95ba084f865f548c8e087f392754b7
Basile Quinchon <basile.quinchon@gmail.com>
2020-02-20 19:35:59 +0100

    better style

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,7 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { initFlatPickr } from "plugins/flatpickr.js";
 import { initRellax } from "plugins/rellax.js";
+import { filterTabs } from "plugins/filter_tabs.js";
 import { initNavbar } from "plugins/init_navbar.js";
 import { mixBlend } from "plugins/mix_blend.js";
 import { pauseVideo } from "plugins/pause_video.js";
@@ -32,3 +33,8 @@ if (document.querySelector('.rellax')) {
 };
 
 AOS.init();
+
+if (document.querySelector('.nav-link')) {
+  filterTabs();
+}
+
