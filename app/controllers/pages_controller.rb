93e5f2f254d4988eb8fbd3e1e775ec672e453fc6
Basile Quinchon <basile.quinchon@gmail.com>
2020-02-20 20:07:41 +0100

    priority

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @projects = Project.all
+    @projects = Project.all.order('priority desc')
     @message = Message.new
   end
 
   def components
-    @projects = Project.all
+    @projects = Project.all.order('priority desc')
   end
 end
