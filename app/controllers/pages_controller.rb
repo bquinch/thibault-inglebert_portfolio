fbfaa9910a2e15759cf78ebb9c13e7e41287cf09
Basile Quinchon <basile.quinchon@gmail.com>
2020-04-19 14:37:30 +0200

    oauth on render

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :render_modal]
 
   def home
     @projects = Project.all.order('priority desc')
